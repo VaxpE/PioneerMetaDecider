315f9a4b0f67fc624e88996c41d2b4fc3b90807f
VaxpE <89207608+VaxpE@users.noreply.github.com>
2023-10-28 03:27:38 +0300

    add meta func

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from modules.tournament import tournament
 from modules.Deck import Deck
+from modules.meta import meta
 
 
-meta = open('results.txt', 'w')
-f = open('data.txt', 'r')
-tournament(f, meta)
+
+with open('data.txt', 'r') as tournament_resul:
+    with open('results.txt', 'r') as current_meta:
+        temp = meta(current_meta, tournament_resul)
+f = open('results.txt', 'w+')
+f.seek(0)
 f.close()
-meta.close()
-print('test')
+with open('results.txt', 'w') as current_meta:
+    for deck in temp:
+        print(deck, file = current_meta)
